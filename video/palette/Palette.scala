762aca62dcca41643e59277af02a0082c43c83a5
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2018-03-24 20:40:58 -0400

    Refactor

--- video/palette/Palette.scala
+++ video/palette/Palette.scala
@@ -9,34 +9,48 @@ sealed abstract class Side {
   def col: Int
 }
 object Left extends Side {
-  def col: Int = 0
+  def col = 0
 }
 object Right extends Side {
-  def col: Int = 4
+  def col = 4
+}
+
+sealed abstract class Pole {
+  def row: Int
+}
+object North extends Pole {
+  def row = 0
+}
+object South extends Pole {
+  def row = 4
+}
+
+sealed abstract class Shade {
+  def value: Int
+}
+object Darkest extends Shade {
+  def value = 0
+}
+object Dark extends Shade {
+  def value = 85
+}
+object Light extends Shade {
+  def value = 170
+}
+object Lightest extends Shade {
+  def value = 255
 }
 
 object Main {
   private val width = 50
   private val img = new BufferedImage(width * 8, width * 8, BufferedImage.TYPE_INT_RGB)
-  private val a = 0
-  private val b = 85
-  private val c = 170
-  private val d = 255
-  private val shades = List(a, b, c, d)
+  private val shades = List(Darkest, Dark, Light, Lightest)
 
   def main(args: Array[String]): Unit = {
-    for ((green, index) <- shades.zipWithIndex) {
-      fourBlues(a, green, Left, index)
-    }
-    for ((green, index) <- shades.zipWithIndex) {
-      fourBlues(b, green, Right, index)
-    }
-    for ((green, index) <- shades.zipWithIndex) {
-      fourBlues(c, green, Left, 4 + index)
-    }
-    for ((green, index) <- shades.zipWithIndex) {
-      fourBlues(d, green, Right, 4 + index)
-    }
+    fourByFour(Darkest, Left, North)
+    fourByFour(Dark, Right, North)
+    fourByFour(Light, Left, South)
+    fourByFour(Lightest, Right, South)
     val pngFile = new File("palette.png")
     val gifFile = new File("palette.gif")
     ImageIO.write(img, "png", pngFile)
@@ -52,9 +66,15 @@ object Main {
     }
   }
 
-  def fourBlues(red: Int, green: Int, side: Side, row: Int): Unit = {
+  def fourByFour(red: Shade, side: Side, pole: Pole): Unit = {
+    for ((green, index) <- shades.zipWithIndex) {
+      fourBlues(red, green, side, pole.row + index)
+    }
+  }
+
+  def fourBlues(red: Shade, green: Shade, side: Side, row: Int): Unit = {
     for ((blue, index) <- shades.zipWithIndex) {
-      square(side.col + index, row, new Color(red, green, blue))
+      square(side.col + index, row, new Color(red.value, green.value, blue.value))
     }
   }
 }
